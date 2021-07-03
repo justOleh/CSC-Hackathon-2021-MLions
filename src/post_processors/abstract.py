3f5167a30cdaa4288afe940a3545dc4d68d435ed
Author <Author>
2021-07-04 02:16:16 +0300

    add some partial implementation blur removal

--- src/post_processors/abstract.py
+++ src/post_processors/abstract.py
@@ -1,13 +1,14 @@
 from abc import ABC, abstractmethod
 import yaml
+from pathlib import Path
 
 
 class AbstractPostProcessor(ABC):
 
     def __init__(self, input_path: str, output_path: str, config_path: str):
-        self.input_path = input_path
-        self.output_path = output_path
-        self.config_path = config_path
+        self.input_path = Path(input_path)
+        self.output_path = Path(output_path)
+        self.config_path = Path(config_path)
         self.config = self.load_config(config_path)
 
     def load_config(self, config_path: str) -> dict:
--- src/post_processors/blur_removal.py
+++ src/post_processors/blur_removal.py
@@ -1,9 +1,30 @@
 from src.post_processors.abstract import AbstractPostProcessor
+import torch
+import cv2 as cv
+import numpy as np
 
 
 class BlurRemoval(AbstractPostProcessor):
     def __init__(self, input_path: str, output_path: str, config_path: str = 'configs/blur_removal.yml'):
         super().__init__(input_path, output_path, config_path)
+
+        self.transforms = None
+        self.classes = ['clear', 'blur']
+        model = self._def_model()
+        self.model = self._load_model(model, self.config['weights'])
+
+    def _def_model(self) -> torch.Model:
+        pass
+
+    def _preprocess_img(self, img: np.ndarray) -> torch.Tensor:
+        pass
+
+    def _load_model(self, model: torch.Model, weights_path: str) -> torch.Model:
+        pass
+
+    def _model_inference(self, model: torch.Model, img: np.ndarray) -> str:
+        return 'clear'
+        # return 'blur'
 
     def process(self):
         pass
