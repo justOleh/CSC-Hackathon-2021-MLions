87361b21e7b0f92e0625ea452143deeb03f22657
Oleh <34406675+justOleh@users.noreply.github.com>
2021-07-03 12:50:45 +0300

    little refactoring

--- blur_classifier.py
+++ blur_classifier.py
@@ -15,21 +15,14 @@ transforms.Compose([
          ]),
  
 model_ft = models.resnet18(pretrained=True)
-for param in model_ft.parameters():
-    param.requires_grad = False
     
 num_ftrs = model_ft.fc.in_features
-print(num_ftrs)
-# Here the size of each output sample is set to 2.
-# Alternatively, it can be generalized to nn.Linear(num_ftrs, len(class_names)).
 model_ft.fc = nn.Linear(num_ftrs, 2)
-
 
 img = cv.imread('drive/MyDrive/CSC_Hackathon_2021_MLions/IMG_2739.png')
 # img = cv.imread('IMG_2749.png')
 img_preproc = torch.unsqueeze(data_transforms['val'](img), dim=0)
 
-class = torch.argmax(model_ft(img_preproc.to(device)))
-
+pred_class = torch.argmax(model_ft(img_preproc.to(device)))
 
 # weights - CSC_Hackathon_2021_MLions/blur_classifier.pth
