a905bca2bc67dbfb2a709fcb5741f76905c38122
k.dubska <k.dubska@abtollc.com>
2021-07-04 15:42:36 +0300

    Added segmentator call to bash script

--- run.sh
+++ run.sh
@@ -4,3 +4,4 @@
 
 python3 main.py --input_path data/blur --output_path data/blur --mode None --post_process blur
 python3 main.py --input_path data/find_people --output_path data/find_people --mode find_people --post_process None
+python3 main.py --input_path data/image_samples/test --output_path data_samples/segment_results --mode segment --post_process None
